ea3fae1d1d36b08a49c1c50b3f5f5c6de540d506
leewu5 <leewu5@163.com>
2025-04-18 19:03:00 +0800

    删除不必要文件

--- src/main.rs
+++
@@ -1,8 +0,0 @@
-use leetcode::solutions::p0002_add_two_numbers::{display, ListNode};
-
-fn main() {
-    let a = Some("1".to_string());
-    let b = &a;
-    // println!("{}", a.unwrap()); 
-    println!("{}", b.as_ref().unwrap());
-}
