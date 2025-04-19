17f9c6238c3057b942e108fff6a8ee71bd0ee5dd
leewu5 <leewu5@163.com>
2025-04-20 00:57:39 +0800

    完成快排，完成快排测试

--- src/lib.rs
+++ src/lib.rs
@@ -10,14 +10,33 @@ pub mod utils;
 
 #[cfg(test)]
 pub mod tests {
-    use crate::solutions::p0002_add_two_numbers::ListNode;
+    use crate::utils::quick_sort;
 
     pub mod p0001_two_sum;
     pub mod p0002_add_two_numbers;
     pub mod p0003_length_of_longest_substring;
 
+    #[derive(PartialEq, Debug)]
+    struct lee {
+        x: i32,
+        y: i32
+    }
+
+    impl lee {
+        pub fn new(x: i32, y: i32) -> Self {
+            lee { x, y }
+        }
+    }
+
     #[test]
     pub fn test_my_thought() {
-        assert_eq!(ListNode::from_vec(vec![1, 2]), ListNode::from_vec(vec![1, 2]));
+        let mut v = vec![3, 1, 4, 2, 9];
+        quick_sort(&mut v, &|x, y| x < y);
+        assert_eq!(v, vec![1, 2, 3, 4, 9]);
+        quick_sort(&mut v, &|x, y| x > y);
+        assert_eq!(v, vec![9, 4, 3, 2, 1]);
+        let mut v = vec![lee::new(1, 2), lee::new(2, 1)];
+        quick_sort(&mut v, &|x: &lee, y: &lee| x.x < y.x);
+        assert_eq!(v, vec![lee::new(1, 2), lee::new(2, 1)]);
     }
 }
--- src/utils.rs
+++ src/utils.rs
@@ -12,3 +12,24 @@ pub fn binary_search<T: Ord>(items: &Vec<T>, target: &T, left: usize, right: usi
     }
     return Some(mid);
 }
+
+pub fn quick_sort<T, F>(items: &mut [T], cmp: &F) 
+where 
+    F: Fn(&T, &T) -> bool,
+{
+    let n = items.len();
+    if (n < 2) {return;}
+    // pivot归位
+    let mut j = 0;
+    for i in 1..n {
+        if cmp(&items[i], &items[0]) {
+            j += 1;
+            items.swap(i, j);
+        }
+    }
+    items.swap(0, j);
+    // 分片递归
+    let (l, r) = items.split_at_mut(j);
+    quick_sort(l, cmp);
+    quick_sort(&mut r[1..], cmp);
+}
